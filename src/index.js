b9ac9b35bfbd84714a4494cbc85ad7e3d09e37eb
Moongirl1207 <amber.perard@student.howest.be>
2019-10-16 11:02:00 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {
+    devineLog,
+    sum
+} from './js/utility/helpers.js';
+
+{
+
+    const init = () => {
+        console.log(`hello Wereld, ik ben Amber - ik ga mee naar San Fransisco!`);
+        devineLog(`Ik gebruik deze functie. Dit is de som ${sum(2, 33)}`);
+    }
+    init();
+}
---
+++ src/js/utility/helpers.js
@@ -0,0 +1,4 @@
+export const devineLog = logThis => console.log(`Devine log: ${logThis}`);
+export const sum = (a, b) => {
+    return a + b;
+}
